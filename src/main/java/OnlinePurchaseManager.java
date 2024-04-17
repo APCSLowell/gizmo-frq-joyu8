bf22d6d29f6be82cbdb880b938672b0715040084
joyu8 <142926471+joyu8@users.noreply.github.com>
2024-04-16 23:15:44 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -20,7 +20,15 @@ public class OnlinePurchaseManager
   }
   public boolean hasAdjacentEqualPair()
   {
-    
+    if(purchases.size() == 2)
+      return false;
+    for(int i = 0; i < purchases.size() - 1; i++){
+      Gizmo meep = purchases.get(i);
+      Gizmo meep2 = purchases.get(i + 1);
+      if(meep.equals(meep2))
+          return true;
+    }
+    return false;
   }
   public OnlinePurchaseManager()
   {
